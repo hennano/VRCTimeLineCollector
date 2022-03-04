c3b6b843a5302836138edf0ad0065bc3d5a1817a
hennano <pittyama37@gmail.com>
2022-03-04 23:39:42 +0900

    作成、更新日時の追加

--- src/main/kotlin/net/hennabatch/vrctimeline/collector/db/entity/PlayerEntity.kt
+++ src/main/kotlin/net/hennabatch/vrctimeline/collector/db/entity/PlayerEntity.kt
@@ -10,13 +10,20 @@ data class PlayerEntity(
     @KomapperColumn(name = "player_id")
     val id: Int,
 
+    @KomapperColumn(name = "vrc_player_id")
+    val vrcPlayerId: String = "",
+
     @KomapperColumn(name = "name")
     val name: String,
 
     @KomapperColumn(name = "is_friend")
-    val isFriend: Boolean,
+    val isFriend: Boolean = false,
 
     @KomapperCreatedAt
     @KomapperColumn(name = "created_at")
     val createdAt: LocalDateTime? = null,
+
+    @KomapperUpdatedAt
+    @KomapperColumn(name = "updated_at")
+    val updatedAt: LocalDateTime?= null
 )
